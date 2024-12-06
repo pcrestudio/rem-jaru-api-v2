9a7bfcb348486b02861c7915c6938ea32abbf57c
pcrestudio <piero.cusi@estudiorodrigo.com>
2024-12-06 14:10:19 -0800

    feat: modify cors and port

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3005);
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "*",
+    credentials: false,
+  });
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>("PORT") || 8000;
+  await app.listen(port);
 }
 bootstrap();
